30c2daeabc3806c96b389670c6d0c3d90cbfdaf0
Ankit <rajankit26smp@gmail.com>
2025-03-11 17:15:59 +0530

    User schema done and implemented hashing the password using bcryptjs

---
+++ backend/src/models/user.schema.js
@@ -0,0 +1,56 @@
+import mongoose  from "mongoose";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema(
+    {
+        firstName : {
+            type : String,
+            required : [true, 'FirstName is required'],
+            maxLength : [20,"FirstName should be less than 20 chars"],
+            trim : true
+        },
+        lastName : {
+            type : String,
+            required : [true,'LastName is required'],
+            maxLength : [20,"LastName should be less than 20 chars"],
+            trim : true
+        },
+        email :{
+            type : String,
+            required : true,
+            unique : true,
+            match: [
+                /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 
+                "Please enter a valid email address"
+            ]
+        },
+        password : {
+            type : String,
+            required : true,
+            minLength : [6,"Password length should be minimum of 6 chars"],
+            maxLength : [14,'Password should not exceed 14 chars'],
+            select : false
+        },
+        balance : {
+            type :Number,
+            default : 1000
+        }
+    },
+    {
+    timestamps : true
+    }
+)
+
+// Hashing the password before saving
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password'))return next()
+    try {
+        this.password = await bcrypt.hash(this.password,10)
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default mongoose.model("User",userSchema)
